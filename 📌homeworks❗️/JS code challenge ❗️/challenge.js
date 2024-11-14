25fed3ff3bd509919c248db23c14a10655703118
zeynepyilmaz <ylmzbhr1@gmail.com>
2024-11-14 18:19:15 +0300

    do_it

---
+++ 📌homeworks❗️/JS code challenge ❗️/challenge.js
@@ -0,0 +1,7 @@
+// 5-Elektrik Faturası Hesaplama (Loop ve Conditional)
+// Bir kişinin elektrik tüketimine göre fatura hesaplayan bir fonksiyon yaz. Elektrik birim fiyatı 0.5 TL ve her 100 birimden sonra ekstra %5 vergi ekleniyor. Kullanıcıdan aylık tüketimini al ve 100 birimlik aralıklar halinde faturasını hesapla. Sonucunda, tüm aralıklar ve toplam maliyet çıktı olarak verilsin.
+
+// 6. Yıllık Faiz Hesaplama (Loop ve Function)
+// Bir miktarın yıllık bileşik faizini hesaplayan bir fonksiyon yaz. Başlangıç miktarını, faiz oranını ve yıl sayısını kullanıcıdan alarak her yılın sonunda birikmiş toplam miktarı göster. Bileşik faiz, her yıl sonu miktarına eklenerek hesaplanır.
+
+// 7.Bir arkadaşınız, uzun bir metnin içinde gizli bir kelime olduğunu söylüyor ve bu kelime büyük harfle yazılı. Metin içinde sadece büyük harfleri alarak gizli kelimeyi bulan bir fonksiyon yazın.
