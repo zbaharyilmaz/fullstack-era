8f946a16db6db07d0d534be223c0f0beb3f1528d
zeynepyilmaz <ylmzbhr1@gmail.com>
2024-12-03 20:35:32 +0300

    update javascript!

--- teamwork/score.js
+++ teamwork/score.js
@@ -15,9 +15,36 @@ button1.onclick= function(){
         game.score1= game.score1 +1;
         scoreFirst.textContent=game.score1
 
-        if(game.score1>= scoreAim){
-            alert()
-            bu
+        if(game.score1>= game.scoreAim){
+            alert("The winner is Player 1!")
+            button1.disabled=true;
+            button2.disabled=true;
+            scoreFirst.classList.add("winner");
+            scoreSec.classList.remove("winner");
         }
     }
+}
+button2.onclick= function(){
+    if(game.scoreAim>0){
+        game.score2= game.score2 +1;
+        scoreSec.textContent=game.score2
+
+        if(game.score1>= game.scoreAim){
+            alert("The winner is Player 2!")
+            button1.disabled=true;
+            button2.disabled=true;
+            scoreSec.classList.add("winner");
+            scoreFirst.classList.remove("winner");
+        }
+    }
+}
+resetButton.onclick= function(){
+    game.score1=0;
+    game.score2=0;
+    game.scoreAim=0;
+    ScoreFirst.textContent=game.score1;
+    ScoreSec.textContent=game.score2;
+    inputElement.valur= "";
+    button1.disabled=false;
+    button2.disabled=false;
 }
