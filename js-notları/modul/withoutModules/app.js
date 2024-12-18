c6ca49b73a8cc5755307d8a70ffb9caa69ea574e
zeynepyilmaz <ylmzbhr1@gmail.com>
2024-12-18 21:31:06 +0300

    "without modules" tutorial notes have taken

---
+++ js-notları/modul/withoutModules/app.js
@@ -0,0 +1,27 @@
+console.log("Selam app.");
+
+
+document.querySelector("h1").textContent="without modules"
+
+
+
+const products = [
+    {
+      name: "İphone15",
+      price: 2000,
+    },
+    {
+      name: "İphone14",
+      price: 1800,
+    },
+    {
+      name: "Samsung Galaxy",
+      price: 2500,
+    },
+  ];
+  
+  
+  console.log(calculate(products));
+
+
+//alt:üstteki sayfa alttakini görür.alttakinin üstekini görmesi için parametre göndermek gerekir.
---
+++ js-notları/modul/withoutModules/calculate.js
@@ -0,0 +1,12 @@
+console.log("Selam cal.");
+
+
+let myName="ipek"
+console.log(myName);
+
+const calculate=(dizi)=>{
+dizi.reduce((sum, product)=>sum+product.price,0)
+}
+
+
+//üst:üstteki sayfa alttakini görür.alttakinin üstekini görmesi için parametre göndermek gerekir.
