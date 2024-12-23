ba00f1d47a3ee3b0a7ea66f708959319eff920d9
zeynepyilmaz <ylmzbhr1@gmail.com>
2024-12-23 23:50:04 +0300

    js codes completed.

--- Coin App/coin-App-inclass/CoinApp/src/getCoin.js
+++ Coin App/coin-App-inclass/CoinApp/src/getCoin.js
@@ -1,6 +1,10 @@
 
+
 // export const getCoin = (query) => {
 //     console.log("getcoin çalıştı",query)
+
+import axios from "axios";
+import displayCoin from "./displayCoin";
 
 //     const url = `https://api.coinranking.com/v2/coins?search=${query}`
 //     const options = {
@@ -40,6 +44,7 @@
 //default export sayfada sadece bir tane yapılır. ya da export const getCoin=()=>{.... şeklinde de export yapılır. Bunu tercih edersen iport {getCoin} from ... şeklinde import yapmalısın.
 
 //NODEİJS ORTAMINDA THİRD PARTY KÜTÜPHANE OLARAK AXIOS
+
 export const getCoin = async (query) => {
      console.log("getcoin çalıştı", query);
     
@@ -51,6 +56,13 @@ export const getCoin = async (query) => {
            "coinrankingec14ee28abb96867e04943e463a2d704f333db7526b7be88",
        },
      };
-     const res = await axios(url,options)
-     console.log(res);
+    const res= await axios(url, options)
+    console.log(res.data.data.coins);
+    if(!res.data.data.coins.length){
+      console.log("coin bulunamadı");
+    }
+    else{
+      //console.log("coin bulundu", res.data.data.coins[0].name);
+      displayCoin(res.data.data.coins[0])
+    }
     }
