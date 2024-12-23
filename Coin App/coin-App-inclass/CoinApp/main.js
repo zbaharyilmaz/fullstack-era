24ac0450ec685f24dce9f23e5bd484083917163d
zeynepyilmaz <ylmzbhr1@gmail.com>
2024-12-23 13:13:41 +0300

    js codes update

--- Coin App/coin-App-inclass/CoinApp/main.js
+++ Coin App/coin-App-inclass/CoinApp/main.js
@@ -2,8 +2,26 @@ import "./scss/style.scss"
 
 const form= document.querySelector("header form")
 //console.log(form);
+import {getCoin} from "./src/getCoin"
 
 form.addEventListener("submit", (e)=>{
-    e.preventDefault()    //yazının 
-    console.log("form tetikte");
+    e.preventDefault()    //sayfa render ediyor. konsoldaki yazı çıkıyor ama kayboluyor. engellemek için.
+    //console.log("form tetikte");
+    getInputVal()
+    form.reset()   //yakaladıktan sonra input alanı resetleniyor.
+      //ya da console.log(e.target); e.target.reset()
+      //e.target, olayın gerçekleştiği DOM elemanını döndürür.Genellikle form, input, buton gibi elemanların olaylarını izlemek için kullanılır. Konsola e.target yazdırarak, hangi elemanın olayın hedefi olduğunu görebilirsiniz.
+  
 })
+const getInputVal=()=>{
+    const inputVal= document.querySelector("header form input").value   //bunu en yukarda tanımlasaydık, boş gelirdi konsolda.
+    console.log(inputVal);
+    if(!inputVal.trim()){
+        alert("input boş bırakılamaz");
+    }
+    else{
+        console.log("input dolu", inputVal);
+        getCoin(inputVal)
+    }
+  
+}
